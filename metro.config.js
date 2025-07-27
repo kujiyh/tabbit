77fe320ffa602dfe8486e58eb2f698c94d004203
kujiyh <121921365+kujiyh@users.noreply.github.com>
2025-07-26 20:55:13 -0700

    Updated metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,5 +1,11 @@
 const { getDefaultConfig } = require('expo/metro-config');
+
 const config = getDefaultConfig(__dirname);
+
+// Add .wasm to asset extensions
 config.resolver.assetExts.push('wasm');
+
+// Add .wasm to source extensions
 config.resolver.sourceExts.push('wasm');
+
 module.exports = config;
